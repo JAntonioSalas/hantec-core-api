6c58fdf0b9ea5c3ad0865b19235a47d2e8596698
Antonio Salas <jsalas@dotkomma.mx>
2025-11-19 17:07:40 -0600

    [FIX] manifest: revert version to 18.0.0.0.1

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Hantec API Ecommerce",
     "summary": "Hantec API Ecommerce",
-    "version": "18.0.15.0.1",
+    "version": "18.0.0.0.1",
     "author": "C&O PROJECTS AND SOLUTIONS",
     "license": "OEEL-1",
     "category": "Sales Management",
