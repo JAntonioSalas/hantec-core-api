75ebb5686d8f0e411eec62019c17cee34eb2a073
Antonio Salas <jsalas@dotkomma.mx>
2026-01-05 12:59:59 -0600

    [FIX] manifest: add missing dependencies for l10n_mx_edi and l10n_mx_edi_stock

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     "author": "C&O PROJECTS AND SOLUTIONS",
     "license": "OEEL-1",
     "category": "Sales Management",
-    "depends": ["sale_management"],
+    "depends": ["sale_management", "l10n_mx_edi", "l10n_mx_edi_stock"],
     "installable": True,
     "development_status": "Production/Stable",
     "maintainers": ["C&O PROJECTS AND SOLUTIONS"],
