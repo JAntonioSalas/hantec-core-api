a7c466d4c0192274bbddc4e9b010d0512d33c2a5
Antonio Salas <jsalas@dotkomma.mx>
2026-01-05 12:34:42 -0600

    [FIX] manifest: revert version number to 18.0.0.0.0 and remove unnecessary dependencies

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Hantec Core API",
     "summary": "Hantec Core API",
-    "version": "18.0.0.0.1",
+    "version": "18.0.0.0.0",
     "author": "C&O PROJECTS AND SOLUTIONS",
     "license": "OEEL-1",
     "category": "Sales Management",
-    "depends": ["sale_management", "l10n_mx_edi_document", "l10n_mx_edi_stock"],
+    "depends": ["sale_management"],
     "installable": True,
     "development_status": "Production/Stable",
     "maintainers": ["C&O PROJECTS AND SOLUTIONS"],
