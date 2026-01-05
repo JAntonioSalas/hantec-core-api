647e2f72e472beb38bb389a54ad38eb14ebfa7a5
Antonio Salas <jsalas@dotkomma.mx>
2026-01-05 12:25:29 -0600

    [IMP] manifest: update version number to 18.0.0.0.1 and adjust dependencies

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Hantec Core API",
     "summary": "Hantec Core API",
-    "version": "18.0.0.0.0",
+    "version": "18.0.0.0.1",
     "author": "C&O PROJECTS AND SOLUTIONS",
     "license": "OEEL-1",
     "category": "Sales Management",
-    "depends": ["sale_management", "l10n_mx_edi_autosent_edi_document", "l10n_mx_edi_stock"],
+    "depends": ["sale_management", "l10n_mx_edi_document", "l10n_mx_edi_stock"],
     "installable": True,
     "development_status": "Production/Stable",
     "maintainers": ["C&O PROJECTS AND SOLUTIONS"],
